6a18ba51c1dffe677bb76488138a8005d3708926
chuang-rueitang <dodododo.0419@gmail.com>
2025-11-27 11:36:13 +0800

    4

---
+++ data.js
@@ -0,0 +1,254 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "內部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6131869367246363,
+          "pitch": 0.41733526664527254,
+          "rotation": 0.7853981633974483,
+          "target": "5-5"
+        },
+        {
+          "yaw": -3.1319744449346594,
+          "pitch": 0.22977978031480717,
+          "rotation": 0,
+          "target": "4-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "內部2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6959864027767075,
+          "pitch": 0.18095857719446684,
+          "rotation": 6.283185307179586,
+          "target": "2-3"
+        },
+        {
+          "yaw": 2.993601546092817,
+          "pitch": 0.10986728463064566,
+          "rotation": 7.0685834705770345,
+          "target": "3-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.29134118434888734,
+          "pitch": -0.5260605045008315,
+          "title": "橫濱鋼彈",
+          "text": "<font color=\"#0a0a0a\" face=\"Google Sans, Roboto, Arial, sans-serif\"><span style=\"font-size: 16px;\">高18公尺的1:1比例RX-78F00初代鋼彈，是設施的核心展示，每半小時會進行約10分鐘的移動及動作表演。</span></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-3",
+      "name": "內部3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18568971711879279,
+          "pitch": 0.2269533405896933,
+          "rotation": 0.7853981633974483,
+          "target": "1-2"
+        },
+        {
+          "yaw": -1.739019185762153,
+          "pitch": 0.19103108815469838,
+          "rotation": 0,
+          "target": "4-4"
+        },
+        {
+          "yaw": 1.6145170066730667,
+          "pitch": 0.05168161222550083,
+          "rotation": 7.0685834705770345,
+          "target": "3-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-1",
+      "name": "內部1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0119112251235762,
+          "pitch": 0.3517370364370933,
+          "rotation": 5.497787143782138,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-4",
+      "name": "內部4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.5599009151505623,
+        "pitch": 0.4778140925750254,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8567316488606309,
+          "pitch": 0.36724813277690593,
+          "rotation": 0.7853981633974483,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-5",
+      "name": "內部5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0034188472046548,
+          "pitch": 0.4035329125831968,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

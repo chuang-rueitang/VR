5b4e6975b42dcb41e6da0956ee9f2ad96cb9d808
chuang-rueitang <dodododo.0419@gmail.com>
2025-12-04 11:51:38 +0800

    Update data.js

--- home/data.js
+++ home/data.js
@@ -37,6 +37,6 @@ var APP_DATA = {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
